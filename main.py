6e06eb1b4b06bb740799960f886eb9a07217adc8
Daniel Oliveras <danioliveras7@gmail.com>
2022-05-28 21:15:08 +0200

    Coge input de textarea y lo envia con un periodo a la consola

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
+from sqlite3 import Time
 import eel
 from numpy import block
+import time
 
+from pandas import period_range
+
+period = 0.5 
+lastSend = time.time()
 msg = ""
 
 
-def takeit(text):
-    print("Unagas "+text)
+
+def send(msg):
+    print(msg)
+
+def addToMsg(text):
+    global msg
+    msg+=text
+    global lastSend 
+    if(time.time()-lastSend > period):
+        send(msg)
+        msg =""
+        lastSend = time.time()
 
 @eel.expose
 def getText(text):
-    takeit(text)
+    addToMsg(text)
 
 eel.init('web')
-eel.start('./templates/index.html', mode='chrome', cmdline_args=['--kiosk'])
-
+eel.start('./templates/index.html', mode='chrome-app', port=8080, cmdline_args=['--start-fullscreen', '--browser-startup-dialog'])
